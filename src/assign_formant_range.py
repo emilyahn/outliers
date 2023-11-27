b8db13a6f6e88a2e40e82c8a3325ee8c58268a73
Emily Ahn <eahn@wellesley.edu>
2023-11-27 14:39:52 -0800

    update assign formants script (no hardcode paths), add train files for script

--- src/assign_formant_range.py
+++ src/assign_formant_range.py
@@ -8,11 +8,18 @@ from statistics import mean
 from sklearn.covariance import MinCovDet
 
 
-# assign Common Voice speakers to high or low formant range
-# to run:
-# python assign_formant_range.py
-# ex:
-# python src/assign_formant_range.py > data/cv8/formants/settings_highlow.tsv
+""" Assign Common Voice speakers to high or low formant range,
+		using SciKitLearn MinCovDet package and Mahalanobis distance.
+	Read in 2 training files (given in Github repo).
+	Apply over a pair of TSV 'test' formant files.
+	Print to Standard Out.
+
+	To run:
+		python assign_formant_range.py {train_low_tsv} {train_high_tsv} {test_low_formant_tsv} {test_high_formant_tsv}
+	ex:
+		python src/assign_formant_range.py data/old_cv7/train_cv7_gender_low_mp.tsv data/old_cv7/train_cv7_gender_high_mp.tsv data/hausa_formants_low4.csv data/hausa_formants_high4.csv > data/cv8/formants/settings_highlow.tsv
+	NOTE for Github: first 2 train files are given in Repo. Test 2 files can be found on OSF.
+"""
 
 # TRAIN: load 1202 data points across 11 langs (uzbek -> dutch)
 def process_gender_tsv(tsv_file):
@@ -42,67 +49,78 @@ def process_gender_tsv(tsv_file):
 			f_f2.append(f2[i])
 	return m_f1, m_f2, f_f1, f_f2
 
-low_m_f1, low_m_f2, low_f_f1, low_f_f2 = process_gender_tsv('data/old_cv7/train_cv7_gender_low_mp.tsv')
-high_m_f1, high_m_f2, high_f_f1, high_f_f2 = process_gender_tsv('data/old_cv7/train_cv7_gender_high_mp.tsv')
+# collect f1_mid and f2_mid values into dictionaries per speaker
+def get_spkr_formants(formant_infile):
+	print(formant_infile, file=sys.stderr)
+	spkr_formants = {}
 
-# fit male speakers to LOW setting
-male_f12 = np.stack((np.array(low_m_f1), np.array(low_m_f2)), axis=1)
-low_cov = MinCovDet(random_state=0).fit(male_f12)
-# fit female speakers to HIGH setting
-female_f12 = np.stack((np.array(high_f_f1), np.array(high_f_f2)), axis=1)
-high_cov = MinCovDet(random_state=0).fit(female_f12)
+	# 2 versions of code. Exactly same except for diff in loading encoding
+	try:
+		with open(formant_infile, encoding='utf-16') as csvfile:
+			reader = csv.DictReader(csvfile, delimiter=',')
 
-# TEST: iterate across langs
-print('lang\tspkr\tsetting\tF1 avg\tF2 avg')
-for low_formant_infile in glob.glob('data/cv8/*/*_formants_low2.*'):
-# do 1 lang at a time version:
-# for low_formant_infile in glob.glob('data/comvoi/formants/full/hungarian_formants_low4.csv'):
-	high_formant_infile = low_formant_infile.replace('low2', 'high2')
+			for row in reader:
+
+				fileid = row['file']
+				spkr_id = fileid.split('_')[0]  # ex. 00004
+
+				# average formant midpoint with its nearest +/- 10ms around the midpoint
+				f1 = mean([float(row['f1_mid']), float(row['f1_mp1']), float(row['f1_mp2'])])
+				f2 = mean([float(row['f2_mid']), float(row['f2_mp1']), float(row['f2_mp2'])])
+
+				if spkr_id not in spkr_formants:
+					spkr_formants[spkr_id] = defaultdict(list)
+
+				spkr_formants[spkr_id]['f1'].append(f1)
+				spkr_formants[spkr_id]['f2'].append(f2)
+
+	except:
+		# remove encoding
+		with open(formant_infile) as csvfile:
+			reader = csv.DictReader(csvfile, delimiter=',')
+
+			for row in reader:
+				fileid = row['file']
+				spkr_id = fileid.split('_')[0]  # ex. 00004
+
+				# average formant midpoint with its nearest +/- 10ms around the midpoint
+				f1 = mean([float(row['f1_mid']), float(row['f1_mp1']), float(row['f1_mp2'])])
+				f2 = mean([float(row['f2_mid']), float(row['f2_mp1']), float(row['f2_mp2'])])
+
+				if spkr_id not in spkr_formants:
+					spkr_formants[spkr_id] = defaultdict(list)
+
+				spkr_formants[spkr_id]['f1'].append(f1)
+				spkr_formants[spkr_id]['f2'].append(f2)
+
+	return spkr_formants
+
+
+def main():
+	train_low_tsv = sys.argv[1]  # e.g. 'data/old_cv7/train_cv7_gender_low_mp.tsv'
+	train_high_tsv = sys.argv[2]  # e.g. 'data/old_cv7/train_cv7_gender_high_mp.tsv'
+	low_formant_infile = sys.argv[3]  # e.g. data/hausa_formants_low4.csv
+	high_formant_infile = sys.argv[4]  # e.g. data/hausa_formants_high4.csv
+
+	low_m_f1, low_m_f2, low_f_f1, low_f_f2 = process_gender_tsv(train_low_tsv)
+	high_m_f1, high_m_f2, high_f_f1, high_f_f2 = process_gender_tsv(train_high_tsv)
+
+	# TRAIN step:
+	# fit male speakers to LOW setting
+	male_f12 = np.stack((np.array(low_m_f1), np.array(low_m_f2)), axis=1)
+	low_cov = MinCovDet(random_state=0).fit(male_f12)
+	# fit female speakers to HIGH setting
+	female_f12 = np.stack((np.array(high_f_f1), np.array(high_f_f2)), axis=1)
+	high_cov = MinCovDet(random_state=0).fit(female_f12)
+
+	# TEST: iterate across langs
+	print('lang\tspkr\tsetting\tF1 avg\tF2 avg')
+	# for low_formant_infile in glob.glob('data/cv8/*/*_formants_low2.*'):
+	# do 1 lang at a time version:
+	# for low_formant_infile in glob.glob('data/comvoi/formants/full/hungarian_formants_low4.csv'):
+
+	# high_formant_infile = low_formant_infile.replace('low2', 'high2')
 	lang = os.path.basename(low_formant_infile).split('_')[0]  # makes "sorbian_upper" into "sorbian"
-
-	# collect f1_mid and f2_mid values into dictionaries per speaker
-	def get_spkr_formants(formant_infile):
-		print(formant_infile, file=sys.stderr)
-		spkr_formants = {}
-		try:
-			with open(formant_infile, encoding='utf-16') as csvfile:
-				reader = csv.DictReader(csvfile, delimiter=',')
-
-				for row in reader:
-
-					fileid = row['file']
-					spkr_id = fileid.split('_')[0]  # ex. 00004
-
-					# average formant midpoint with its nearest +/- 10ms around the midpoint
-					f1 = mean([float(row['f1_mid']), float(row['f1_mp1']), float(row['f1_mp2'])])
-					f2 = mean([float(row['f2_mid']), float(row['f2_mp1']), float(row['f2_mp2'])])
-
-					if spkr_id not in spkr_formants:
-						spkr_formants[spkr_id] = defaultdict(list)
-
-					spkr_formants[spkr_id]['f1'].append(f1)
-					spkr_formants[spkr_id]['f2'].append(f2)
-
-		except:
-			# remove encoding
-			with open(formant_infile) as csvfile:
-				reader = csv.DictReader(csvfile, delimiter=',')
-
-				for row in reader:
-					fileid = row['file']
-					spkr_id = fileid.split('_')[0]  # ex. 00004
-
-					# average formant midpoint with its nearest +/- 10ms around the midpoint
-					f1 = mean([float(row['f1_mid']), float(row['f1_mp1']), float(row['f1_mp2'])])
-					f2 = mean([float(row['f2_mid']), float(row['f2_mp1']), float(row['f2_mp2'])])
-
-					if spkr_id not in spkr_formants:
-						spkr_formants[spkr_id] = defaultdict(list)
-
-					spkr_formants[spkr_id]['f1'].append(f1)
-					spkr_formants[spkr_id]['f2'].append(f2)
-
-		return spkr_formants
 
 	low_spkr_formants = get_spkr_formants(low_formant_infile)
 	high_spkr_formants = get_spkr_formants(high_formant_infile)
@@ -127,3 +145,7 @@ for low_formant_infile in glob.glob('data/cv8/*/*_formants_low2.*'):
 
 		out_str = f"{lang}\t{spkr}\t{label}\t{print_f1:.2f}\t{print_f2:.2f}"
 		print(out_str)
+
+
+if __name__ == '__main__':
+	main()
